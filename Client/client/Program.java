8218b740c640a812d81a61ba4a5153ad6ab06473
= <kong.arve@gmail.com>
2013-03-14 15:00:09 +0100

    add login window

--- Client/client/Program.java
+++ Client/client/Program.java
@@ -1,13 +1,52 @@
 package client;
 
-import gui.CalendarView;
+import gui.LoginWindow;
 
 import javax.swing.JFrame;
 
 public class Program {
-
-	public static void main(String[] args) {
-		CalendarView window = new CalendarView();
+	private ClientConnection conn;
+	private LoginWindow loginWindow;
+	Program(){
+		loginWindow = new LoginWindow(this);
+	}	
+	
+	public void login(String hoststr, String username, String password){
+		String hostname = "";
+		int port = 0;
+		
+		
+		// Client side validation
+		
+		
+		// check for sane host string
+		if(hoststr.contains(":") && hoststr.replaceAll("[^:]", "").length() == 1){ // make sure we contain 1 and only 1 colon character.
+			hostname = hoststr.split(":")[0];
+			port = Integer.parseInt(hoststr.split(":")[1]);
+			
+			
+			System.out.println(hostname);
+			System.out.println(port);
+		} else{
+			loginWindow.alert("Invalid hostname. <ip>:<port>");
+			System.out.println(hostname.contains(":"));
+			System.out.println( hostname.replaceAll("[^:]", "").length());
+			return;
+		}
+		
+		// Make sure password isn't empty
+		if(password.length()==0){
+			loginWindow.alert("Please type in your password");
+			return;
+		}
+		
+		// ...
+		loginWindow.alert("Login success");
 		
 	}
+	
+	public static void main(String[] args) {
+		new Program();	
+	}
+	
 }
---
+++ Client/gui/LoginWindow.java
@@ -0,0 +1,132 @@
+package gui;
+
+import static java.awt.GridBagConstraints.*;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import client.ClientConnection;
+import client.Program;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.prefs.Preferences;
+
+public class LoginWindow extends JFrame{
+	// Default values for host and username
+	Preferences prefs;
+	Program mainProgram;
+
+	private JLabel alert = new JLabel("Velkommen!");
+	private JTextField host = new JTextField();
+	private JTextField username = new JTextField();
+	private JPasswordField password = new JPasswordField();
+	
+	private JButton loginButton = new JButton("Login");
+	
+	
+	private JLabel hostLabel = new JLabel("Host:port");
+	private JLabel usernameLabel = new JLabel("E-post");
+	private JLabel passwordLabel = new JLabel("Password");
+	
+	private JPanel pane = new JPanel(new GridBagLayout());
+	
+	public LoginWindow(Program mainProgram){
+		// Call super to create window with title
+		super("Login");
+		this.mainProgram = mainProgram;
+		setVisible(false);
+		setSize(300, 180);
+	    doTheLayout();
+		getContentPane().add(pane);
+	    setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Get the screen size  
+		GraphicsConfiguration gc = getGraphicsConfiguration();  
+		Rectangle bounds = gc.getBounds();  
+		
+		// Set the Location and Activate  
+		Dimension size = getPreferredSize();  
+		setLocation((int) ((bounds.width / 2) - (size.getWidth() / 2)),  
+		                    (int) ((bounds.height / 2) - (size.getHeight() / 2)));
+
+		setResizable(false);
+		setVisible(true);  
+		
+		loginButton.addActionListener(new buttonListener());
+	}
+	
+	public void alert(String input){
+		alert.setText(input);
+	}
+	
+	
+	private void doTheLayout(){
+	    prefs = Preferences.userRoot().node(this.getClass().getName());
+	    host.setText(prefs.get("HOST", ""));
+	    username.setText(prefs.get("USERNAME", ""));
+		
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(3, 7, 3, 7);
+		c.gridwidth = 2;
+		pane.add(alert, c);
+		c.gridwidth = 1;
+		c.gridx = 0;
+		
+		c.anchor = LINE_END;
+			c.gridy = 1;
+			pane.add(hostLabel, c);
+
+			c.gridy = 2;
+			pane.add(usernameLabel, c);
+
+			c.gridy = 3;
+			pane.add(passwordLabel, c);
+		c.gridx=1;
+		c.anchor = LINE_START;
+		c.weightx = 1.0d;
+		c.fill = HORIZONTAL;
+			c.gridy = 1;
+			pane.add(host, c);
+
+			c.gridy = 2;
+			pane.add(username, c);
+
+			c.gridy = 3;
+			pane.add(password,c);
+			c.gridy = 4;
+			
+			c.gridy = 5;
+			pane.add(loginButton, c);
+	}
+	
+	// Action Listener for Login button
+	class buttonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			Object src = e.getSource();
+			if( src.equals(loginButton)){
+				System.out.println("login event");
+				// Save host and username
+				prefs.put("USERNAME", username.getText());
+				prefs.put("HOST", host.getText());
+				
+				String pass = password.getPassword().toString();
+				try {
+					mainProgram.login(host.getText(), username.getText(), pass);
+				} catch (Exception e1) {
+					alert(e1.getMessage());
+					
+				}
+			
+			}	
+		}
+	}
+	
+}
