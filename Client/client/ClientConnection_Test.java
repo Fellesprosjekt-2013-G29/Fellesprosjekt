7b761e5792c82d81c4e45589cb86245a7bbd10ad
Tor-Håkon Bonsaksen <torrib90@gmail.com>
2013-03-14 12:39:42 +0100

    Changed IP for testing

--- Client/client/ClientConnection_Test.java
+++ Client/client/ClientConnection_Test.java
@@ -6,7 +6,7 @@ import structs.Response;
 
 public class ClientConnection_Test 
 {
-	private final static String HOST = "localhost";
+	private final static String HOST = "78.91.10.195";
 	private final static int PORT = 4447;
 	
 	public static void main(String[] args) throws Exception
